97c5b7a19b34003b817ef6552c3e8a564b757555
aamarrazziq <A176882@siswa.ukm.edu.my>
2021-11-10 00:32:39 +0800

    commit first

--- app/src/main/java/com/example/jomdobi/MainActivity.java
+++ app/src/main/java/com/example/jomdobi/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.jomdobi;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+
+// Using handler with postDelayed called runnable run method
+
+            @Override
+
+            public void run() {
+
+                Intent i = new Intent(MainActivity.this, Signup.class);
+
+                startActivity(i);
+
+                // close this activity
+
+                finish();
+
+            }
+
+        }, 5*1000); // wait for 5 seconds
+
     }
 }
